0031c084e960faa9b7386405e39566ad812472c6
Yuxu (6*7) <godjira.9017@gmail.com>
2025-07-21 21:05:38 +0100

    Much More Scalable

---
+++ game_objects.py
@@ -0,0 +1,53 @@
+from PySide6 import QtWidgets, QtGui, QtCore
+
+class Game_Object (QtWidgets.QGraphicsPixmapItem):
+	def __init__ (self, img, sizeXY):
+		super().__init__( QtGui.QPixmap(img.scaled(sizeXY[0], sizeXY[1])) )
+		self.setShapeMode(QtWidgets.QGraphicsPixmapItem.BoundingRectShape)
+		self.og_image = img
+		self.w = sizeXY[0]
+		self.h = sizeXY[1]
+		self.speed = 0
+		self.score_add = 0
+	
+	def width (self):
+		return self.w
+		
+	def height (self):
+		return self.h
+	
+	def setSpeed (self, num):
+		self.speed = num
+	
+	def setScoreAdd (self, num):
+		self.score_add = num
+	
+	def __str__ (self):
+		return f"Game Object : \nheight: {self.h}\nwidth: {self.w}\nx: {self.x()}\ny: {self.y()}\nspeed: {self.speed}\nscore add: {self.score_add}\n"
+		
+	def __repr__ (self):
+		return self.__str__()
+	
+		
+class Player (Game_Object):
+	def __init__ (self, img, sizeXY):
+		super().__init__( img, sizeXY )
+		self.speed = 10
+		
+	def shape (self):
+		path = QtGui.QPainterPath()
+		path.addRect(0, 0, self.w, self.h * (3/8))
+		
+		return path
+
+class Fruit (Game_Object):
+	def __init__ (self, img, sizeXY):
+		super().__init__( img, sizeXY )
+		self.score_add = 10
+		self.speed = 2
+	
+	def shape (self):
+		path = QtGui.QPainterPath()
+		path.addRect(0, 0, self.w, self.h)
+		
+		return path
--- interface.py
+++ interface.py
@@ -1,24 +1,24 @@
 from PySide6 import QtWidgets, QtCore, QtGui
+from game_objects import Player, Fruit
 from random import choice
 
 class Interface (QtWidgets.QWidget):
 	def __init__ (self):
 		super().__init__()
-		self.squr_x = 0
-		self.squr_y = 288
+		self.player_x = 0
+		self.player_y = 288
 		
-		self.fall_coords = [i for i in range (0, 385, 50)]
-		self.fall = None
+		self.fruit_coords = [i for i in range (0, 385, 50)]
+		self.fruit = None
 		self.vbox = None
 		self.stop_box = None
 		
 		self.bg_color = QtGui.QColor(0, 181, 226)
-		self.squr_img = QtGui.QImage ("./textures/basket.png").scaled(96,96)
-		self.fall_img = QtGui.QImage ("./textures/apple.png").scaled(48,48)
+		self.player_img = QtGui.QImage ("./textures/basket.png")
+		self.fruit_img = QtGui.QImage ("./textures/apple.png")
 		self.stop_color = QtGui.QColor (127, 127, 127, 170)
 		
-		self.score_boost = 0
-		self.speed = 2
+		self.fruit_speed = None
 		self.current_key = None
 		self.score_num = 0
 		self.display_text = f"<font size=5>Score : {self.score_num}</font>"
@@ -47,11 +47,12 @@ class Interface (QtWidgets.QWidget):
 		self.view = QtWidgets.QGraphicsView (scene = self.scene)
 		self.view.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
 		
-		self.squr = self.scene.addPixmap(QtGui.QPixmap(self.squr_img))
-		self.squr.setShapeMode(QtWidgets.QGraphicsPixmapItem.BoundingRectShape) 	
 		self.vbox.addWidget (self.score)
-		self.squr.setPos (self.squr_x, self.squr_y)
 		
+		self.player = Player (self.player_img, (96,96))
+		self.player.setPos (self.player_x, self.player_y)
+		
+		self.scene.addItem (self.player)
 		self.vbox.addWidget (self.view)
 		
 	def keyPressEvent (self, e):
@@ -60,82 +61,74 @@ class Interface (QtWidgets.QWidget):
 	def keyReleaseEvent (self, e):
 		self.current_key = None
 
-	def show_score (self, negative = 1):
-		self.score_num = self.score_num + (self.score_boost * negative)
+	def show_score (self, score_add):
+		self.score_num = self.score_num + score_add
 		self.display_text = f"<font size=5>Score : {self.score_num}</font>"
 		self.score.setText (self.display_text)
 		self.score.update ()
 		
 	def game_tick (self):
-		if (self.current_key == QtCore.Qt.Key_Q or self.current_key == QtCore.Qt.Key_A) and self.squr_x > 0:
-			self.squr_x -= 10
+		if (self.current_key == QtCore.Qt.Key_Q or self.current_key == QtCore.Qt.Key_A) and self.player_x > 0:
+			self.player_x -= self.player.speed
 
-		elif self.current_key == QtCore.Qt.Key_D and self.squr_x < (self.scene.width() - 96):
-			self.squr_x += 10
+		elif self.current_key == QtCore.Qt.Key_D and self.player_x < (self.scene.width() - 96):
+			self.player_x += self.player.speed
 		
-		self.squr.setX(self.squr_x)
+		self.player.setX(self.player_x)
 		
-		if self.fall == None :
-			self.fall_x = choice (self.fall_coords)
-			self.fall = self.scene.addPixmap(QtGui.QPixmap(self.fall_img))
-			self.fall.setShapeMode(QtWidgets.QGraphicsPixmapItem.BoundingRectShape)
-			self.fall.setX (self.fall_x)
-			self.fall.setY (-48)
+		if self.fruit == None :
+			self.fruit_x = choice (self.fruit_coords)
+			self.fruit = Fruit (self.fruit_img, (48,48))
+			self.scene.addItem (self.fruit)
+			self.fruit.setX (self.fruit_x)
+			self.fruit.setY (-48)
 			
-		elif self.fall.collidesWithItem(self.squr):
-			self.scene.removeItem (self.fall)
-			self.fall = None
-			self.score_boost += 1
-			self.show_score()
+			if self.fruit_speed == None:
+				self.fruit_speed = self.fruit.speed
 			
-			if self.speed < 8:
-				self.speed += 2
+		elif self.fruit.collidesWithItem(self.player):
+			self.show_score(self.fruit.score_add)
+			self.scene.removeItem (self.fruit)
+			self.fruit = None
 			
-		elif self.fall.pos().y() >= self.scene.height():
-			self.scene.removeItem (self.fall)
-			self.fall = None
-			self.show_score(negative = -1)
+			if self.fruit_speed < 8:
+				self.fruit_speed += 2
 			
-			if self.speed > 2:
-				self.speed -= 2
+		elif self.fruit.pos().y() >= self.scene.height():
+			self.scene.removeItem (self.fruit)
+			self.fruit = None
+			self.show_score(0)
+			
+			if self.fruit_speed > 2:
+				self.fruit_speed -= 2
 		else:
-			self.fall.setY (self.fall.pos().y() + self.speed)
+			self.fruit.setY (self.fruit.pos().y() + self.fruit_speed)
 			
 	def stop_game (self):
 		self.game_timer.stop()
-		
 		if self.stop_box == None:
 			self.stop_box = self.scene.addRect(0, 0, self.scene.width(), self.scene.height(), QtGui.QPen(self.stop_color), QtGui.QBrush(self.stop_color))
 		
 	def start_game (self):
 		self.game_timer.start()
-		
 		if self.stop_box != None:
 			self.scene.removeItem (self.stop_box)
 			self.stop_box = None
 		
 	def reset_game (self):
 		self.game_timer.stop()
-		
 		if self.stop_box != None:
 			self.scene.removeItem (self.stop_box)
 			self.stop_box = None
-			
-		self.scene.removeItem (self.fall)
-		self.fall = None
 		
-		self.scene.removeItem (self.squr)
+		self.scene.removeItem (self.fruit)
+		self.fruit = None
 		
-		self.squr_x = 0
+		self.player_x = 0
+		self.player.setPos (self.player_x, self.player_y)
 		
-		self.squr = self.scene.addPixmap(QtGui.QPixmap(self.squr_img))
-		self.squr.setShapeMode(QtWidgets.QGraphicsPixmapItem.BoundingRectShape) 	
-		self.squr.setPos (self.squr_x, self.squr_y)
-		
-		self.score_boost = 0
 		self.score_num = 0
-		self.show_score()
-		
-		self.speed = 2
+		self.show_score(0)
+		self.fruit_speed = None
 		
 		self.start_game()
