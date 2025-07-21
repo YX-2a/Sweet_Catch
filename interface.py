ea9865b61edab9be0a7a711aa9dbf7d1d9ab39c8
Yuxu (6*7) <godjira.9017@gmail.com>
2025-07-21 04:44:54 +0100

    Some Comfort Features

--- interface.py
+++ interface.py
@@ -5,19 +5,20 @@ class Interface (QtWidgets.QWidget):
 	def __init__ (self):
 		super().__init__()
 		self.squr_x = 0
-		self.squr_y = 300
-		self.fall_coords = [i for i in range (0, 401, 50)]
+		self.squr_y = 288
+		
+		self.fall_coords = [i for i in range (0, 385, 50)]
 		self.fall = None
 		self.vbox = None
 		self.stop_box = None
 		
 		self.bg_color = QtGui.QColor(0, 181, 226)
-		self.squr_img = QtGui.QImage ("./textures/basket.png")
-		self.fall_img = QtGui.QImage ("./textures/apple.png")
+		self.squr_img = QtGui.QImage ("./textures/basket.png").scaled(96,96)
+		self.fall_img = QtGui.QImage ("./textures/apple.png").scaled(48,48)
 		self.stop_color = QtGui.QColor (127, 127, 127, 170)
 		
 		self.score_boost = 0
-		self.current_intv = 50
+		self.speed = 2
 		self.current_key = None
 		self.score_num = 0
 		self.display_text = f"<font size=5>Score : {self.score_num}</font>"
@@ -25,15 +26,9 @@ class Interface (QtWidgets.QWidget):
 	def make (self):
 		self.make_widgets ()
 		
-		self.mov_timer = QtCore.QTimer()
-		self.mov_timer.setInterval (15)
-		self.mov_timer.timeout.connect (self.player_move)
-		
-		self.fall_timer = QtCore.QTimer()
-		self.speed = 0
-		self.current_intv = self.current_intv - self.speed
-		self.fall_timer.setInterval (self.current_intv)
-		self.fall_timer.timeout.connect (self.fall_move)
+		self.game_timer = QtCore.QTimer()
+		self.game_timer.setInterval (16)
+		self.game_timer.timeout.connect (self.game_tick)
 		
 		self.start_game()
 		
@@ -47,15 +42,16 @@ class Interface (QtWidgets.QWidget):
 		self.score.setTextFormat (QtCore.Qt.RichText)
 		self.score.setAlignment (QtCore.Qt.AlignHCenter)
 		
-		self.scene = QtWidgets.QGraphicsScene (0,0,500,400)
+		self.scene = QtWidgets.QGraphicsScene (0,0,480,384)
 		self.scene.setBackgroundBrush(QtGui.QBrush(self.bg_color))
-		self.view = QtWidgets.QGraphicsView (scene = self.scene)	
+		self.view = QtWidgets.QGraphicsView (scene = self.scene)
+		self.view.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
 		
 		self.squr = self.scene.addPixmap(QtGui.QPixmap(self.squr_img))
 		self.squr.setShapeMode(QtWidgets.QGraphicsPixmapItem.BoundingRectShape) 	
-		self.squr.setPos (self.squr_x,self.squr_y)
-		
 		self.vbox.addWidget (self.score)
+		self.squr.setPos (self.squr_x, self.squr_y)
+		
 		self.vbox.addWidget (self.view)
 		
 	def keyPressEvent (self, e):
@@ -63,29 +59,28 @@ class Interface (QtWidgets.QWidget):
 	
 	def keyReleaseEvent (self, e):
 		self.current_key = None
-	
-	def player_move (self):		
-		if (self.current_key == QtCore.Qt.Key_Q or self.current_key == QtCore.Qt.Key_A) and self.squr_x > 0:
-			self.squr_x -= 10
 
-		elif self.current_key == QtCore.Qt.Key_D and self.squr_x < (self.scene.width() - 100):
-			self.squr_x += 10
-		
-		self.squr.setPos(self.squr_x, self.squr_y)
-				
 	def show_score (self, negative = 1):
-		self.score_num = self.score_num + (self.score_boost) * negative
+		self.score_num = self.score_num + (self.score_boost * negative)
 		self.display_text = f"<font size=5>Score : {self.score_num}</font>"
 		self.score.setText (self.display_text)
 		self.score.update ()
 		
-	def fall_move (self):
-		self.speed = 5
+	def game_tick (self):
+		if (self.current_key == QtCore.Qt.Key_Q or self.current_key == QtCore.Qt.Key_A) and self.squr_x > 0:
+			self.squr_x -= 10
+
+		elif self.current_key == QtCore.Qt.Key_D and self.squr_x < (self.scene.width() - 96):
+			self.squr_x += 10
+		
+		self.squr.setX(self.squr_x)
+		
 		if self.fall == None :
 			self.fall_x = choice (self.fall_coords)
 			self.fall = self.scene.addPixmap(QtGui.QPixmap(self.fall_img))
 			self.fall.setShapeMode(QtWidgets.QGraphicsPixmapItem.BoundingRectShape)
-			self.fall.setPos (self.fall_x,0)
+			self.fall.setX (self.fall_x)
+			self.fall.setY (-48)
 			
 		elif self.fall.collidesWithItem(self.squr):
 			self.scene.removeItem (self.fall)
@@ -93,39 +88,34 @@ class Interface (QtWidgets.QWidget):
 			self.score_boost += 1
 			self.show_score()
 			
-			if self.current_intv > 10:
-				self.current_intv = self.current_intv - self.speed
-				self.fall_timer.setInterval (self.current_intv)
+			if self.speed < 8:
+				self.speed += 2
 			
-		elif self.fall.y() == self.scene.height() - 25:
+		elif self.fall.pos().y() >= self.scene.height():
 			self.scene.removeItem (self.fall)
 			self.fall = None
 			self.show_score(negative = -1)
 			
-			if self.current_intv < 45:
-				self.current_intv = self.current_intv + self.speed
-				self.fall_timer.setInterval (self.current_intv)
+			if self.speed > 2:
+				self.speed -= 2
 		else:
-			self.fall.setPos (self.fall_x,self.fall.pos().y() + 5)
+			self.fall.setY (self.fall.pos().y() + self.speed)
 			
 	def stop_game (self):
-		self.fall_timer.stop()
-		self.mov_timer.stop()
+		self.game_timer.stop()
 		
 		if self.stop_box == None:
 			self.stop_box = self.scene.addRect(0, 0, self.scene.width(), self.scene.height(), QtGui.QPen(self.stop_color), QtGui.QBrush(self.stop_color))
 		
 	def start_game (self):
-		self.fall_timer.start()
-		self.mov_timer.start()
+		self.game_timer.start()
 		
 		if self.stop_box != None:
 			self.scene.removeItem (self.stop_box)
 			self.stop_box = None
 		
 	def reset_game (self):
-		self.fall_timer.stop()
-		self.mov_timer.stop()
+		self.game_timer.stop()
 		
 		if self.stop_box != None:
 			self.scene.removeItem (self.stop_box)
@@ -137,17 +127,15 @@ class Interface (QtWidgets.QWidget):
 		self.scene.removeItem (self.squr)
 		
 		self.squr_x = 0
-		self.squr_y = 300
 		
 		self.squr = self.scene.addPixmap(QtGui.QPixmap(self.squr_img))
 		self.squr.setShapeMode(QtWidgets.QGraphicsPixmapItem.BoundingRectShape) 	
-		self.squr.setPos (self.squr_x,self.squr_y)
-			
+		self.squr.setPos (self.squr_x, self.squr_y)
+		
 		self.score_boost = 0
 		self.score_num = 0
-		self.current_intv = 50
-		
-		self.fall_timer.setInterval (self.current_intv)
 		self.show_score()
+		
+		self.speed = 2
 		
 		self.start_game()
--- window.py
+++ window.py
@@ -1,14 +1,39 @@
 from PySide6.QtWidgets import QMainWindow
+from PySide6.QtCore import Qt, QTimer, QEvent
 from interface import Interface
 from menu import Menu
 
 class Window (QMainWindow):
 	def __init__ (self):
-		super().__init__()
+		super().__init__(parent = None, flags = Qt.WindowCloseButtonHint | Qt.WindowMinimizeButtonHint | Qt.WindowTitleHint)
 		self.setWindowTitle ("Fallers")
+		self.setFixedSize(500, 466) 
 		
-		inter = Interface()
-		self.setCentralWidget (inter.make())
+		self.inter = Interface()
+		self.setCentralWidget (self.inter.make())
 		
-		menu = Menu(inter, self)
+		menu = Menu(self.inter, self)
 		self.setMenuBar (menu.make())
+		
+		self.move_timer = QTimer(self)
+		self.move_timer.setInterval(250)
+		self.move_timer.setSingleShot(True)
+		self.move_timer.timeout.connect(self.inter.start_game)
+		
+	def moveEvent(self, e):
+		self.inter.stop_game()
+		self.move_timer.start()
+		super().moveEvent(e)
+		
+	def changeEvent(self, e):
+		if e.type() == QEvent.WindowStateChange or e.type() == QEvent.ActivationChange:
+			if self.isMinimized():
+				self.inter.stop_game()
+				
+			elif self.isActiveWindow():
+				self.inter.start_game()
+				
+			else:
+				self.inter.stop_game()
+		
+		super().changeEvent(e)
